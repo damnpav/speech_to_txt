3b5845cb9fa29dd1b1cf95b18110de7c4911a942
damnpav <hcanilvap@gmail.com>
2025-01-18 14:40:56 +0300

    settings

--- streaming.py
+++ streaming.py
@@ -1,18 +1,21 @@
 import pyaudio
 import queue
+import wave
 import threading
 from google.cloud import speech
 from google.oauth2 import service_account
+from datetime import datetime as dt
 
 client_file = 'speechdemo-447721-8b16a025ef9c.json'
 credentials = service_account.Credentials.from_service_account_file(client_file)
 client = speech.SpeechClient(credentials=credentials)
 
 FORMAT = pyaudio.paInt16  # Audio format (16-bit PCM)
-RATE = 16000              # Sample rate
-CHUNK = int(RATE / 10)    # Frames per buffer
+RATE = 44100              # Sample rate
+CHUNK = 1024   # Frames per buffer
 input_device = 2
 CHANNELS = 1
+WAVE_OUTPUT_FILENAME = f"audio/stream_trans_{dt.now().strftime(f'%H_%M_%S__%Y%m%d')}.wav"
 
 audio_queue = queue.Queue()
 
@@ -34,6 +37,23 @@ def create_audio_stream():
         stream_callback=callback,
     )
     return stream, audio_interface
+
+
+def save_audio_to_file():
+    #print(f"Recording audio to {WAVE_OUTPUT_FILENAME}...")
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(1)
+    wf.setsampwidth(pyaudio.PyAudio().get_sample_size(pyaudio.paInt16))
+    wf.setframerate(RATE)
+
+    while True:
+        chunk = audio_queue.get()
+        if chunk is None:  # End signal
+            break
+        wf.writeframes(chunk)  # Write audio chunk to WAV file
+
+    wf.close()
+    print(f"Audio saved to {WAVE_OUTPUT_FILENAME}")
 
 
 # Streaming transcription
@@ -68,7 +88,14 @@ def main():
     stream, audio_interface = create_audio_stream()
     print("Listening and transcribing...")
 
-    threading.Thread(target=listen_and_transcribe, daemon=True).start()
+    # Thread for saving audio to a file
+    recorder_thread = threading.Thread(target=save_audio_to_file, daemon=True)
+    recorder_thread.start()
+
+    # Thread for real-time transcription
+    transcription_thread = threading.Thread(target=listen_and_transcribe, daemon=True)
+    transcription_thread.start()
+
 
     try:
         stream.start_stream()
@@ -79,5 +106,9 @@ def main():
         stream.stop_stream()
         stream.close()
         audio_interface.terminate()
+        audio_queue.put(None)  # Signal the recorder thread to stop
+        recorder_thread.join()
+        transcription_thread.join()
+        print("Finished.")
 
 main()
